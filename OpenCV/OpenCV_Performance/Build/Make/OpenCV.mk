36db306c501d0eb3e8717d7e00c6da8e74638c58
Florian Kesseler <florian.kesseler@alliedvision.com>
2022-06-09 13:23:05 +0200

    Updated OpenCV package names

--- OpenCV/OpenCV_Performance/Build/Make/OpenCV.mk
+++ OpenCV/OpenCV_Performance/Build/Make/OpenCV.mk
@@ -1,10 +1,10 @@
 include $(MAKE_INCLUDE_DIR)/Common.mk
 
 #Compile options needed for OpenCV
-OPENCV_CFLAGS       = $(shell $(PKGCFG) --cflags opencv)
+OPENCV_CFLAGS       = $(shell $(PKGCFG) --cflags opencv4)
 
 #Linker options needed for OpenCV
-OPENCV_LIBS         = $(shell $(PKGCFG) --libs opencv)
+OPENCV_LIBS         = $(shell $(PKGCFG) --libs opencv4)
 
 #Operations we have to do in order to prepare OpenCV
 OpenCV:
